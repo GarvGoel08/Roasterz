dca2d45ef505df8173f448f901d94b5824ef8009
Garv Goel <garvgoel2927@gmail.com>
2024-10-12 22:04:48 +0530

    removed buttons from scroll

--- src/components/ScrollableContainer.js
+++ src/components/ScrollableContainer.js
@@ -4,29 +4,6 @@ const ScrollableContainer = ({ children }) => {
   const [scrollPosition, setScrollPosition] = useState(0);
   const itemListRef = useRef(null);
 
-  useEffect(() => {
-    const updateButtonVisibility = () => {
-      const container = itemListRef.current;
-      if (!container) return;
-
-      const rightButton = document.querySelector(".scroll-button.right");
-      const leftButton = document.querySelector(".scroll-button.left");
-
-      if (scrollPosition < container.scrollWidth - container.clientWidth) {
-        rightButton.classList.remove("Collapsed");
-      } else {
-        rightButton.classList.add("Collapsed");
-      }
-
-      if (scrollPosition > 0) {
-        leftButton.classList.remove("Collapsed");
-      } else {
-        leftButton.classList.add("Collapsed");
-      }
-    };
-
-    updateButtonVisibility();
-  }, [scrollPosition, children]); // Listen to children to recheck on new renders
 
   const handleScroll = (direction) => {
     const scrollAmount = 160;
@@ -38,41 +15,26 @@ const ScrollableContainer = ({ children }) => {
       } else if (direction === "right") {
         container.scrollLeft += scrollAmount;
       }
-
       setScrollPosition(container.scrollLeft);
     }
   };
 
   return (
-    <div className="ItemList d-flex" style={{ overflowX: "hidden", position: "relative" }}>
-      <button
-        className={`scroll-button left ${scrollPosition > 0 ? "" : "Collapsed"}`}
-        onClick={() => handleScroll("left")}
-        style={{ left: "0", borderRadius: "0px 10px 10px 0px" }}
-      >
-        &lt;
-      </button>
+    <div style={{ position: "relative", overflowX: "hidden" }}>
 
       <div
         ref={itemListRef}
         className="scroll-container"
         style={{
-          overflowX: "hidden",
+          overflowX: "auto",
           scrollbarWidth: "thin",
           position: "relative",
           display: "flex",
+          marginTop: "30px", // Give space for buttons
         }}
       >
         {children}
       </div>
-
-      <button
-        className={`scroll-button right ${scrollPosition < itemListRef.current?.scrollWidth - itemListRef.current?.clientWidth ? "" : "Collapsed"}`}
-        onClick={() => handleScroll("right")}
-        style={{ right: "0" }}
-      >
-        &gt;
-      </button>
     </div>
   );
 };
